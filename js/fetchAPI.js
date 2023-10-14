b24816feca6b527ac6167ae9e8208838e5aca3e1
EdwardObala1 <e.obala@alustudent.com>
2023-10-14 15:56:41 +0200

    fixed the Job experience section

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,28 +1,43 @@
-function fetchAPI(endpoint){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', endpoint, true);
+const cachedData = {};
 
+function fetchAPI(endpoint) {
+  if (cachedData[endpoint] && Date.now() - cachedData[endpoint].timestamp < cacheExpirationTime) {
+    // Data is still valid, use the cached data
+    return Promise.resolve(cachedData[endpoint].data);
+  }
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', endpoint, true);
+
+  return new Promise((resolve, reject) => {
     xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
+      if (xhr.status >= 200 && xhr.status < 300) {
         // Request was successful
-        var responseData = JSON.parse(xhr.responseText);
-        console.log(responseData[0]);
-        return target
-    } else {
+        const responseData = JSON.parse(xhr.responseText);
+        // console.log(responseData);
+        // Cache the fetched data
+        cachedData[endpoint] = {
+          data: responseData,
+          timestamp: Date.now(),
+        };
+        resolve(responseData);
+        return responseData
+      } else {
         // Request failed
         console.error('Request failed with status:', xhr.status);
-    }
+        reject(new Error('Request failed with status: ' + xhr.status));
+      }
     };
 
     xhr.onerror = function () {
-    // There was a network error
-    console.error('Network error occurred');
+      // There was a network error
+      console.error('Network error occurred');
+      reject(new Error('Network error occurred'));
     };
 
     xhr.send();
+  });
 }
-
-fetchAPI('http://localhost:8000/api/experiences')
 
 function cache_results(endpoint){
     var results = fetchAPI(endpoint);
